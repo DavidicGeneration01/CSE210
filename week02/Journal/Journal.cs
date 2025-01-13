558cb737b8a7ded7c4be27d510057a3cf056d4f2
David Alade <aladedavid4@gmail.com>
2025-01-13 19:30:21 +0100

    Journal Design

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile (string File)
+    {
+
+    }
+
+    public void LoadFromFile (string File)
+    {
+        
+    }
+
+}
